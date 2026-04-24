b9efd72872a44e434cfc70558ca17ec06ec8f5b9
oltamorina24 <olta.morina@student.uni-pr.edu>
2026-04-24 14:22:23 +0200

    add user

---
+++ data/users.php
@@ -0,0 +1,5 @@
+<?php
+$users = [
+    ["name"=>"Admin","email"=>"admin@gmail.com","password"=>"admin123","role"=>"admin"],
+    ["name"=>"User","email"=>"user@gmail.com","password"=>"user123","role"=>"user"]
+];
