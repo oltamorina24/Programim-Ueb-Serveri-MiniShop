748a8f006439077171811413b79434002db4ac61
Mirena <mirenahalitii@gmail.com>
2026-04-25 02:27:52 +0200

    add

---
+++ includes/nav.php
@@ -0,0 +1,37 @@
+<?php 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<nav>
+    <a href="index.php">Home</a>
+    <a href="products.php">Products</a>
+    <a href="search.php">Search</a>
+
+    <?php 
+ 
+    if(isset($_SESSION['user'])): 
+    ?>
+        <a href="logout.php" style="color: #ff4d4d; font-weight: bold;">Logout (<?php echo $_SESSION['user']['name']; ?>)</a>
+    <?php else: ?>
+        <a href="login.php" style="color: #2ecc71; font-weight: bold;">Login</a>
+    <?php endif; ?>
+</nav>
+header.php
+<!DOCTYPE html>
+<html>
+<head>//
+    <title>MiniShop</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<header>
+    <h1>🛒 MiniShop</h1>
+</header>
+footer.php
+ </div>
+<footer style="text-align:center; padding:10px;">
+    <p>MiniShop © 2026</p>
+</footer>
+</body>
+</html>
