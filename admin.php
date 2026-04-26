df7be7311383cbd0d816d5ef9acf7d78f5cb60fa
jola-shala <jola.shala@student.uni-pr.edu>
2026-04-26 22:52:51 +0200

    add

--- admin.php
+++ admin.php
@@ -1,3 +1,25 @@
 <?php
-
+session_start();
+$filePath = 'data/products.php';
+if (file_exists($filePath)){
+    require $filePath;
+}else{
+    $products = [];
+}
+if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin'){
+    header("Location: index.php");
+    exit();
+}
+$message = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['shto_produkt'])){
+        $products[] = [
+            'name' => $_POST['pname'],
+            'price' => (float)$_POST['pprice'],
+            'category' => $_POST['pcat'],
+            'image' => ""
+        ];
+        $message = "Produkti u shtua me sukses!";
+    }
+}
 ?>
